555c20f73d76e8934947ef5e8f9b097c6499fe93
Eldon Kuzhyelil <keldon@suyatitech.local>
2016-12-19 18:32:13 +0530

    sdfsd

---
+++ scripts/copyy.sh
@@ -0,0 +1,2 @@
+echo dsf
+copy "D:\Jenkins\workspace\Firstproject\scripts\test.txt" "D:\Jenkins\workspace\Firstproject\scripts\test2.txt"
--- scripts/head_pkg_builder.sh
+++ scripts/head_pkg_builder.sh
@@ -19,14 +19,14 @@ echo Workspace: $WSPACE
 echo Last Commit: $LCOMMIT
 
 #change working directory to the dir for this build
-echo Changing working directory to /var/lib/jenkins/workspace
-cd /var/lib/jenkins/workspace
+echo Changing working directory to "D:\Jenkins\workspace\FirstProject\TestCI2"
+cd "D:\Jenkins\workspace\FirstProject\TestCI2"
 
 # check for the existence of an existing commit file for this
 # project
-SCRIPTFILE='/var/lib/jenkins/workspace/lastcommit/'$BNAME'.txt'
-echo Searching for script file $SCRIPTFILE...
-if [ -e $SCRIPTFILE ]
+SCRIPTFILE="D:\Jenkins\workspace\FirstProject\TestCI2\lastcommit\FirstProject.txt"
+echo Searching for script file $SCRIPTFILE   ...
+if [  $SCRIPTFILE ]
 then
         PREVRSA=$(<$SCRIPTFILE) &&
         echo Found previous SHA $PREVRSA
@@ -34,21 +34,26 @@ then
         # Backup existing Package.xml
         cd $WSPACE/src
         echo changing directoy to $WSPACE
-        cp package.xml{,.bak} &&
-        echo Backing up package.xml to package.xml.bak &&
+		
+        xcopy package.xml{,.bak} &&
+        #xcopy "package.xml" "package.xml.bak"
+		echo Backing up package.xml to package.xml.bak &&
         read -d '' NEWPKGXML <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Package>
 </Package>
-EOF      
+EOF
         echo ===PKGXML===
         echo $NEWPKGXML
         echo Creating new package.xml
         echo $NEWPKGXML > $WSPACE/src/package.xml
-
-        for CFILE in `git diff-tree --no-commit-id --name-only -r $LCOMMIT $PREVRSA`
+		echo 
+        for CFILE in `git diff-tree --no-commit-id --name-only -r head~2`
+		
+		#git diff-tree --no-commit-id --name-only -r head~2
+		#echo cfile "$CFILE"
         do
-                echo Analyzing file `basename $CFILE`
+                echo Analyzing file basename $CFILE
 
                 case "$CFILE"
                 in
@@ -69,37 +74,51 @@ EOF
 
                 if [[ "$TYPENAME" != "UNKNOWN TYPE" ]]
                 then
-                        ENTITY=$(basename "$CFILE")
+						#basename $CFILE
+                        set ENTITY=basename "$CFILE"
+						echo cfile "$CFILE"
+						set FILE="$CFILE"
+						
+						#for /F %i in ("c:\foo\pdf.cls") do @echo %~ni
+						#for /F %%i in ("$CFILE") do @echo %%~ni
+						
+						echo ENTITY NAME1: $ENTITY
                         ENTITY="${ENTITY%.*}"
+						
+						ENTITY="pdf"
                         echo ENTITY NAME: $ENTITY
 
-                        if grep -Fq "$TYPENAME" $WSPACE/src/package.xml
+                        #if grep -Fq "$TYPENAME" $WSPACE/src/package.xml
+						if [ findstr "$TYPENAME" "$WSPACE\src\package.xml"]
                         then
+								echo "$TYPENAME" is inside package.xml
                                 echo Generating new member for $ENTITY
-                                xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/src/package.xml
+                               xml ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/src/package.xml
                         else
+								
+								echo "$TYPENAME" is NOT inside package.xml
                                 echo Generating new $TYPENAME type
-                                xmlstarlet ed -L -s /Package -t elem -n types -v "" $WSPACE/src/package.xml
-                                xmlstarlet ed -L -s '/Package/types[not(*)]' -t elem -n name -v "$TYPENAME" $WSPACE/src/package.xml
+                                xml ed -L -s /Package -t elem -n types -v "" $WSPACE/src/package.xml
+                                xml ed -L -s '/Package/types[not(*)]' -t elem -n name -v "$TYPENAME" $WSPACE/src/package.xml
                                 echo Generating new member for $ENTITY
-                                xmlstarlet ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/src/package.xml
+                                xml ed -L -s "/Package/types[name='$TYPENAME']" -t elem -n members -v "$ENTITY" $WSPACE/src/package.xml
                         fi
                 else
                         echo ERROR: UNKNOWN FILE TYPE $CFILE
                 fi
                 echo ====UPDATED PACKAGE.XML====
-                cat $WSPACE/src/package.xml
+                #type "$WSPACE\src\package.xml"
         done
         echo Saving last RSA Commit
-        echo $LCOMMIT > /var/lib/jenkins/workspace/lastcommit/$BNAME.txt
+        echo $LCOMMIT > "D:\Jenkins\workspace\FirstProject\lastcommit\$BNAME.txt"
         echo Cleaning up Package.xml
-        xmlstarlet ed -L -s /Package -t elem -n version -v "27.0" $WSPACE/src/package.xml
-        xmlstarlet ed -L -i /Package -t attr -n xmlns -v "http://soap.sforce.com/2006/04/metadata" $WSPACE/src/package.xml
+        xml ed -L -s /Package -t elem -n version -v "27.0" $WSPACE/src/package.xml
+        xml ed -L -i /Package -t attr -n xmlns -v "http://soap.sforce.com/2006/04/metadata" $WSPACE/src/package.xml
 
         echo ====FINAL PACKAGE.XML=====
-        cat $WSPACE/src/package.xml
+        #type "$WSPACE\src\package.xml"
 else
         echo No RSA found, default Package.xml will be used
         echo Creating new list commit file
-        echo $LCOMMIT > /var/lib/jenkins/workspace/lastcommit/$BNAME.txt
+        echo $LCOMMIT > "D:\Jenkins\workspace\FirstProject\lastcommit\'$BNAME'.txt"
 fi
